927ebdfd3be0de131e0ca6c1af76f3940e61f65d
Kelly Roberts <105584129+bubstance@users.noreply.github.com>
2022-07-01 22:44:56 -0700

    Update config.sh

--- config.sh
+++ config.sh
@@ -6,7 +6,13 @@ else
     QEMU_IMG=$QEMU_HOME/bin/qemu-img
 fi
 
-QEMU_FLAGS="-display curses -m 2048"
+# first install with
+# QEMU_FLAGS="-display gtk,zoom-to-fit=on -enable-kvm -m 2048"
+# run the installer and then choose option [15] for the graphics mode installation on at least one of the drives
+# then run with
+# QEMU_FLAGS="-display curses -m 2048"
+# REVEL IN DIVINE INTELLECT
+QEMU_FLAGS="-display gtk,zoom-to-fit=on -enable-kvm -m 2048"
 QEMU_IMG_SIZE="512M"
 QEMU_IMG_MOUNT_DIR="./mnt/"
 # Note on the offset: https://www.cloudsavvyit.com/7517/how-to-mount-a-qemu-virtual-disk-image/
